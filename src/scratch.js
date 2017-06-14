1685c56e8ccdd62b25041bf40219469b461e29ec
Mounika <mounikachow4@gmail.com>
2017-06-14 10:41:40 -0500

    Observables from scratch

---
+++ src/scratch.js
@@ -0,0 +1,30 @@
+// Observables from scratch
+import $ from 'jquery';
+import Rx from 'rxjs/Rx';
+
+const source$= new Rx.Observable(observer =>{
+	console.log('Creating Observable');
+
+	observer.next('Hello World');
+	observer.next('Another Value');
+
+	observer.error(new Error('Error: Something went wrong'))
+
+	setTimeout(()=>{
+		observer.next('Yet another value');
+		observer.complete();
+	}, 3000);
+});
+
+source$
+.catch(err => Rx.Observable.of(err))
+.subscribe(
+	x=>{
+		console.log(x);
+	},
+	err=>{
+		console.log(err);
+	},
+	complete=>{
+		console.log('completed');
+	}); 
