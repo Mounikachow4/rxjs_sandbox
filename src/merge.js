0db37a91de22e0497e98eef163211dfeaae4a010
Mounika <mounikachow4@gmail.com>
2017-06-14 10:49:10 -0500

    Merge and concat

---
+++ src/merge.js
@@ -0,0 +1,26 @@
+//Merge & Concat
+import $ from 'jquery';
+import Rx from 'rxjs/Rx';
+
+Rx.Observable.of('Hello')
+	.merge(Rx.Observable.of('Everyone'))
+	.subscribe(x=>console.log(x));
+
+Rx.Observable.interval(2000)
+	.merge(Rx.Observable.interval(500))
+	.take(25)
+	.subscribe(x=>console.log(x));
+
+const source1$=Rx.Observable.interval(2000).map(v=>'Merge1: '+v);
+const source2$=Rx.Observable.interval(500).map(v=>'Merge2: '+v);
+
+Rx.Observable.merge(source1$, source2$)
+	.take(25)
+	.subscribe(x=>console.log(x));
+
+const source1$=Rx.Observable.range(0, 5).map(v=>'Source1: '+v);
+const source2$=Rx.Observable.range(6, 5).map(v=>'Source2: '+v);
+
+Rx.Observable.concat(source1$, source2$)
+	.subscribe(x=>console.log(x));
+
