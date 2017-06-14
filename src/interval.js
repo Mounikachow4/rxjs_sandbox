1018489ba6fc27f257756ab1ee0e4a14cc3d42ea
Mounika <mounikachow4@gmail.com>
2017-06-14 10:46:02 -0500

    Observables with interval, timer and range

---
+++ src/interval.js
@@ -0,0 +1,43 @@
+// Interval, timer, range
+import $ from 'jquery';
+import Rx from 'rxjs/Rx';
+
+const source$ = Rx.Observable.interval(100)
+	.take(5);
+source$.subscribe(
+	x=>{
+		console.log(x);
+	},
+	err=>{
+		console.log(err)
+	},
+	complete=>{
+		console.log('Completed');
+	});
+
+const source$ = Rx.Observable.timer(5000, 2000)
+	.take(5);
+source$.subscribe(
+	x=>{
+		console.log(x);
+	},
+	err=>{
+		console.log(err)
+	},
+	complete=>{
+		console.log('Completed');
+	}); 
+
+const source$ = Rx.Observable.range(25, 100);
+	.take(5);
+source$.subscribe(
+	x=>{
+		console.log(x);
+	},
+	err=>{
+		console.log(err)
+	},
+	complete=>{
+		console.log('Completed');
+	});
+
